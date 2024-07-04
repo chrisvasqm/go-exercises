e83f693f7c099cb16be6525d09edcbf79ea1a163
chrisvasqm <christianvasquezmelo@gmail.com>
2024-07-04 14:08:51 -0400

    Remove alias

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"fmt"
 	"goexercises/exercises"
-	g "goexercises/greet"
+	"goexercises/greet"
 	"goexercises/models"
 )
 
 func main() {
 	chris := models.Person{Name: "Chris", Age: 29}
-	fmt.Println(g.Greet(chris.Name))
+	fmt.Println(greet.Greet(chris.Name))
 
 	for i := 1; i <= 20; i++ {
 		fmt.Println(exercises.FizzBuzz(i))
